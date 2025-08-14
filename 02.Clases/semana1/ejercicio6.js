813bf40e13d053ab55d98053bc27c05697fac754
NadiaRios1986 <rios.nadia.rios@gmail.com>
2025-08-13 21:45:43 -0600

    super

--- 02.Clases/semana1/ejercicio6.js
+++ 02.Clases/semana1/ejercicio6.js
@@ -1,6 +1,15 @@
 const { ask } = require('../helpers/input');
 
 async function main() {
-    const dia = Number(await ask("Ingresa un numero del 1 al 7"));
-    const diaSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
-}   
+    const dia = Number(await ask("Ingresa un numero del 1 al 7:)"));
+    const diasSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+if (dia >=1 && dia <= 7) {
+    console.log(diasSemana[dia - 1]);
+} else {
+    console.log("no es un número válido"); 
+}
+
+
+
+}
+main();
