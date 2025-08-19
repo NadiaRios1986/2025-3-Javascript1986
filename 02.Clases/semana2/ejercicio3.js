3fba91393e0e880052a557b6ec05c4fd5c030f19
NadiaRios1986 <rios.nadia.rios@gmail.com>
2025-08-18 21:47:08 -0600

    yes

--- 02.Clases/semana2/ejercicio3.js
+++ 02.Clases/semana2/ejercicio3.js
@@ -1,34 +1,40 @@
 const { ask } = require('../helpers/input.js');
 
-async function main(){
-    let opcion= "";
+async function main() {
+  let opcion = '';
+  let frutas = [];
 
-    let frutas="";
+  while (opcion !== '3') {
+    opcion = await ask(`¿Que deseas hacer? \n1. Agregar una fruta. \n2. Eliminar una fruta. \n3. Salir.\n`);
 
+    if (opcion === '1') {
+      let frutaNueva = await ask('¿Qué fruta deseas agregar?: ');
+      frutas.push(frutaNueva);
+      console.log('Fruta agregada: ', frutaNueva);
+      console.log(`Frutas disponibles`, frutas);
+    } else if (opcion === '2') {
+      if (frutas.length === 0) {
+        console.log('No hay frutas disponibles');
+        continue;
+      } else {
+        let frutaEliminar = await ask('¿Qué fruta deseas eliminar?: ');
+        let index = frutas.indexOf(frutaEliminar);
 
-    opcion= await ask("¿Qué deseas hacer? \n1. Agregar una fruta \n2. Eliminar una fruta \n3. Salir \nElige una opción: ");
-    while (opcion !== "3") {
-        if(opcion === "1") {
-            let frutaNueva = await ask("¿Qué fruta quieres agregar? ");
-            frutas.push(frutaNueva);
-            console.log("Fruta agregada: ",frutaNueva);
+        if (index !== -1) {
+          frutas.splice(index, 1);
+          console.log(`${frutaEliminar} ha sido eliminada`);
+        } else {
+          console.log('Fruta no encontrada');
+        }
 
-            console.log("Frutas disponibles", frutas);
-       }else if (opcion === "2") {
-        let frutaEliminar = await ask("¿Qué fruta quieres eliminar? ");
-      
-            console.log("No hay frutas ");
-            continue
-
-
-        }else if (opcion === "3") {
-        console.log("Saliendo del programa...");
-
-
-        }else {
-            console.log("Opción no válida, por favor elige una opción del menú.");
-        }       
-}
+        console.log('Frutas disponibles', frutas);
+      }
+    } else if (opcion === '3') {
+      console.log('Gracias');
+    } else {
+      console.log('Opción inválida');
+    }
+  }
 }
 
 main();
