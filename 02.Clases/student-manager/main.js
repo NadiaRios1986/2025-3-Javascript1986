c5fe31446a22528fa263149c514b65974259facb
NadiaRios1986 <rios.nadia.rios@gmail.com>
2025-08-27 20:42:47 -0600

    up

--- 02.Clases/student-manager/main.js
+++ 02.Clases/student-manager/main.js
@@ -1,8 +1,67 @@
-function addStudent(name, age) {
-    const student = {
-        name: name,
-        age: age
-    };
-    students.push(student);
-    console.log(`Student ${name} added.`);
+
+"use strict";
+
+let students = [];
+
+window.onload = function () {
+  const stored = localStorage.getItem("students");
+  if (stored) {
+    students = JSON.parse(stored);
+    displayStudents();
+    updateAverage();
+  }
+}
+
+function addStudent() {
+  const name = document.getElementById("nameInput").value.trim();
+  if (name === "" || name === isNaN) {
+    alert("Debes de capturar un nombre válido");
+    return;
+  }
+  const grade = parseFloat(document.getElementById("gradeInput").value);
+  if (grade > 100 || grade < 0) {
+    alert("Debes de capturar una calificación válida");
+    return;
+  }
+
+  const student = {
+    name,
+    grade,
+    status: grade >= 70 ? "Passed" : "Failed",
+  }
+
+  students.push(student);
+  displayStudents();
+  updateAverage();
+  saveToLocalStorage();
+  document.getElementById("nameInput").value = "";
+  document.getElementById("gradeInput").value = "";
+}
+
+function displayStudents() {
+  const list = document.getElementById("studentList");
+  list.innerHTML = ``;
+
+  for (let i = 0; i < students.length; i++) {
+    const li = document.createElement("li");
+    li.innerHTML = `<p>${students[i].name} - ${students[i].grade} - ${students[i].status}</p>`;
+    list.appendChild(li);
+  }
+}
+
+function updateAverage() {
+  if (students.length === 0) {
+    return;
+  }
+  let total = 0;
+  for (let i = 0; i < students.length; i++) {
+    total = total + students[i].grade;
+  }
+  let average = total / students.length;
+  document.getElementById("averageDisplay")
+    .textContent = `Average Grade: ${average.toFixed(2)}`;
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("students", JSON.stringify(students));
 }
